a374d983a26153ec43b467723e065b0fb9710e50
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2025-03-22 20:45:10 -0300

    .

---
+++ site-simples/app.js
@@ -0,0 +1,4 @@
+'use strict';
+//O JavaScript tenta ser amigável e ele tenta fazer com que o código funcione e forneça uma solução, mesmo que o resultado venha a ser um erro.
+//Para combater essas deficiências, você pode ativar o modo estrito, que reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.
+
